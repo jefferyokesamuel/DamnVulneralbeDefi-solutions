3eeefaba610adfc623ff1cb4694509c497ddd158
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-11-01 02:52:51 +0100

    Update naive-receiver.challenge.js

--- test/naive-receiver/naive-receiver.challenge.js
+++ test/naive-receiver/naive-receiver.challenge.js
@@ -31,7 +31,6 @@ describe('[Challenge] Naive receiver', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */   
-        const deploy = this.pool.connect(deployer)
         await deployer.sendTransaction({ to: deploy.address, value: ETHER_IN_POOL });
         await deployer.sendTransaction({ to: this.pool.address, value: ETHER_IN_POOL });
         const receiver = this.receiver.connect(attacker)
