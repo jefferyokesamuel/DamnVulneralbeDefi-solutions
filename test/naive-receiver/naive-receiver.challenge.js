fe518b3fba165f48a47472573893a52f5419a7a5
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2023-01-23 13:45:48 +0100

    Update naive-receiver.challenge.js

--- test/naive-receiver/naive-receiver.challenge.js
+++ test/naive-receiver/naive-receiver.challenge.js
@@ -31,7 +31,6 @@ describe('[Challenge] Naive receiver', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */   
-        const poolhack = this.pool.connect(attackContract)
         const AttackFactory = await ethers.getContractFactory("AttackNaiveReceiver", attacker)
         
        
