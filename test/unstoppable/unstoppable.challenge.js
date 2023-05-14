ec12d1f6e2a60cdd9bd25f3e01cf15af9f1bfa7f
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2023-05-15 00:03:43 +0100

    Update unstoppable.challenge.js

--- test/unstoppable/unstoppable.challenge.js
+++ test/unstoppable/unstoppable.challenge.js
@@ -40,7 +40,6 @@ describe('[Challenge] Unstoppable', function () {
  
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */
-        await attackContract.transfer(this.pool.address, INITIAL_ATTACKER_TOKEN_BALANCE)
         
     });
 
